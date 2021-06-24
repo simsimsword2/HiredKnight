b9674f7b5467303e3a32e6af00f2b8ffc6342921
simsimsword2 <simsimsword@gmail.com>
2021-06-24 10:38:18 +0200

    spring-boot

---
+++ src/test/java/com/HN/HiredKnight/HiredKnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HN.HiredKnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiredKnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
