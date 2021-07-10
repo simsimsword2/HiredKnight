a7ecdc6f1f568be9095eedade2ced88d23738df0
simsimsword2 <simsimsword@gmail.com>
2021-07-10 09:53:01 +0200

    cleanUP

--- src/main/java/com/HN/HiredKnight/HiredKnightApplication.java
+++ src/main/java/com/HN/HiredKnight/HiredKnightApplication.java
@@ -3,7 +3,6 @@ package com.HN.HiredKnight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
 
 @SpringBootApplication
 public class HiredKnightApplication {
